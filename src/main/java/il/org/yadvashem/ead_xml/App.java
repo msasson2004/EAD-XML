38734b726b4cab969a47350cc0d81ff87fca0a3f
msasson2004 <msasson2004@yahoo.com>
2017-08-17 09:38:27 +0300

    Modifications: 1. reading input directory and running all files from there. 2. moving mapping file to top level. 3. modifiying controlaccess tags correctly.

--- src/main/java/il/org/yadvashem/ead_xml/App.java
+++ src/main/java/il/org/yadvashem/ead_xml/App.java
@@ -2,11 +2,6 @@ package il.org.yadvashem.ead_xml;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.input.SAXBuilder;
@@ -19,12 +14,12 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String table_mappings, folder_input, result_file, file_name;
+    	String table_mappings, folder_input, result_file;
+    	File[] listOfFiles;
 
-    	if(args.length > 2) {
+    	if(args.length > 1) {
     		folder_input = args[0];
-        	table_mappings = folder_input + "/" + args[1];
-        	file_name = folder_input + "/" + args[2];
+        	table_mappings = args[1];
         	String out_folder = folder_input + "_result";
         	File theDir = new File(out_folder);
         	
@@ -32,6 +27,8 @@ public class App
         	{
         		if(!theDir.exists())
         			theDir.mkdir();
+        		File folder = new File(folder_input);
+        		listOfFiles = folder.listFiles();
         	}
         	catch(Exception e) {
         		System.out.println(e.getMessage());
@@ -40,7 +37,7 @@ public class App
         	result_file = out_folder + "/" + args[2];        	
     	}
     	else {
-    		System.out.println("Syntax: javaw <mapping file> <input dir> <file name>");
+    		System.out.println("Syntax: java App.class <mapping file> <input dir>");
     		return;
     	}
     	 
@@ -50,47 +47,76 @@ public class App
 	    	Mappings map = new Mappings(table_mappings);
 	    	HashMap<String, SubjectTerms> hash = map.build();
 	    	SAXBuilder saxBuilder = new SAXBuilder();
-	        Document document = saxBuilder.build(new File(file_name));
-	        Element root = document.getRootElement();
-	        System.out.println("root is: " + root.getName());
-	        Element arch = getChild(root, "archdesc");
-	        if(arch != null) {
-	        	Element did = getChild(arch, "did");
-	        	if(did != null) {
-	        		Element origin = getChild(did, "origination");
-	        		if(origin != null) {
-	        			String name = origin.getText();
-	        			SubjectTerms st = hash.get(name);
-	        			if(st != null) {
-		        			System.out.println("Found subject: " + st.Subject + " index: " + st.Index + " term: " + st.Term);
-		        			// set the authfilenumber attribute of origination
-		        			origin.setAttribute("authfilenumber", st.Index);
-		        			// create controlaccess tags
-/*		        	        Element ctrlAccess = new Element("controlaccess");
-		        	        Element subject = new Element("subject").setText("somehing");
-		        	        ctrlAccess.addContent(subject);
-		        	        subject = new Element("subject").setText("somehing2");
-		        	        ctrlAccess.addContent(subject);
-		        	        arch.addContent(ctrlAccess);
-		        	        ctrlAccess = new Element("controlaccess");
-		        	        subject = new Element("geogname").setText("Germany");
-		        	        ctrlAccess.addContent(subject);
-		        	        arch.addContent(ctrlAccess);
-		        	        ctrlAccess = new Element("controlaccess");
-		        	        subject = new Element("corpname").setText(st.Subject);
-		        	        ctrlAccess.addContent(subject);
-		        	        arch.addContent(ctrlAccess); */
-		        	// 		write the xml file   
-		        			System.out.println("writing xml file");
-		        			XMLOutputter xmlOutput = new XMLOutputter();
-		        	        xmlOutput.setFormat(Format.getPrettyFormat());
-		        	        xmlOutput.output(document, new FileOutputStream(result_file)); 
-	        			}
-	        		}
-	        	}
-	        }
+	    	for(int i=0;i < listOfFiles.length;i++) 
+	    	{
+	    		if (listOfFiles[i].isFile()) 
+	    		{	    	        
+			        System.out.println("working on: " + listOfFiles[i].getName());
+			        Document document = saxBuilder.build(listOfFiles[i]);
+			        Element root = document.getRootElement();
+			        Element arch = getChild(root, "archdesc");
+			        if(arch != null) {
+			        	Element did = getChild(arch, "did");
+			        	if(did != null) {
+			        		Element origin = getChild(did, "origination");
+			        		if(origin != null) {
+			        			String name = origin.getText();
+			        			SubjectTerms st = hash.get(name);
+			        			if(st != null) {
+				        			System.out.println("Found subject: " + st.Subject + " index: " + st.Index + " term: " + st.Term);
+				        			// set the authfilenumber attribute of origination
+				        			origin.setAttribute("authfilenumber", st.Index);
+				        			origin.setAttribute("source", st.Term);
+				        			// find controlaccess tag
+				        			List<Element> list = arch.getChildren();
+				        			for(int j=0;j<list.size();j++) 
+				        			{
+				        				Element ctrlAccess = list.get(j);
+					        	        if(ctrlAccess != null && ctrlAccess.getName() == "controlaccess") 
+					        	        {
+					        	        	List<Element> children = ctrlAccess.getChildren();
+					        	        	for(int k=0;k<children.size();k++) 
+					        	        	{			
+					        	        		Element elem = children.get(k);
+					        	        		if(elem.getName() == "subject") {
+					        	        			name = elem.getText();
+					        	        			st = hash.get(name);
+					        	        			if(st != null) {
+					        	        				elem.setAttribute("authfilenumber", st.Index);
+					        	        				elem.setAttribute("source", st.Term);					        	        				
+					        	        			}
+					        	        		}
+					        	        		else if(elem.getName() == "geogname") {
+					        	        			name = elem.getText();
+					        	        			st = hash.get(name);
+					        	        			if(st != null) {
+					        	        				elem.setAttribute("authfilenumber", st.Index);
+					        	        				elem.setAttribute("source", st.Term);					        	        				
+					        	        			}
+					        	        		}
+					        	        		if(elem.getName() == "corpname") {
+					        	        			name = elem.getText();
+					        	        			st = hash.get(name);
+					        	        			if(st != null) {
+					        	        				elem.setAttribute("authfilenumber", st.Index);
+					        	        				elem.setAttribute("source", st.Term);					        	        				
+					        	        			}
+					        	        		}
+					        	        	}
+					        	        }
+				        			}
+				        	// 		write the xml file   
+				        			System.out.println("writing xml file");
+				        			XMLOutputter xmlOutput = new XMLOutputter();
+				        	        xmlOutput.setFormat(Format.getPrettyFormat());
+				        	        xmlOutput.output(document, new FileOutputStream(result_file)); 
+			        			}
+			        		}
+			        	}
+			        }
+	    		}
+	    	}
 	        System.out.println("Done");
-	        
 	    } 
 	    catch (FileNotFoundException e) {
 	    	System.out.println(e.getMessage());
@@ -115,6 +141,5 @@ public class App
         		return child;
         }
         return null;
-    }
-    
+    }   
 }
